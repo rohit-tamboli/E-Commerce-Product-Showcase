10e7589a7457dcdd6f5a3951c82d2c10b785a716
Rohit Tamboli <tambolirohit2412@gmail.com>
2025-10-02 23:51:36 +0530

    Your commit message

--- products-data.js
+++ products-data.js
@@ -1,39 +1,46 @@
-// products-data.js
-window.allProducts = [
-  {
-    id: "smartwatch1",
-    name: "Smart Watch",
-    price: 120, 
-    image: "https://images.unsplash.com/photo-1637160151663-a410315e4e75?q=80&w=774&auto=format&fit=crop",
-    page: "product-smartwatch.html"
-  },
-  {
-    id: "camera1",
-    name: "Camera",
-    price: 250,
-    image: "https://images.unsplash.com/photo-1603208234872-619ffa1209cb?q=80&w=748&auto=format&fit=crop",
-    page: "product-camera.html"
-  },
-  {
-    id: "mobile1",
-    name: "Mobile",
-    price: 60000,
-    image: "https://images.unsplash.com/photo-1592899677977-9c10ca588bbd?q=80&w=2913&auto=format&fit=crop",
-    page: "product-mobile1.html"
-  },
-  {
-    id: "mobile2",
-    name: "Mobile",
-    price: 50000,
-    image: "https://images.unsplash.com/photo-1600087626014-e652e18bbff2?q=80&w=928&auto=format&fit=crop",
-    page: "product-mobile2.html"
-  },
-  {
-    id: "Headphones1",
-    name: "Headphones",
-    price: 4999,
-    image: "https://images.unsplash.com/photo-1628202926206-c63a34b1618f?q=80&w=774&auto=format&fit=crop",
-    page: "product-Headphones.html"
-  }
+// // products-data.js
+// window.allProducts = [
+//   {
+//     id: "smartwatch1",
+//     name: "Smart Watch",
+//     price: 120, 
+//     image: "https://images.unsplash.com/photo-1637160151663-a410315e4e75?q=80&w=774&auto=format&fit=crop",
+//     page: "product-smartwatch.html"
+//   },
+//   {
+//     id: "camera1",
+//     name: "Camera",
+//     price: 250,
+//     image: "https://images.unsplash.com/photo-1603208234872-619ffa1209cb?q=80&w=748&auto=format&fit=crop",
+//     page: "product-camera.html"
+//   },
+//   {
+//     id: "mobile1",
+//     name: "Mobile",
+//     price: 60000,
+//     image: "https://images.unsplash.com/photo-1592899677977-9c10ca588bbd?q=80&w=2913&auto=format&fit=crop",
+//     page: "product-mobile1.html"
+//   },
+//   {
+//     id: "mobile2",
+//     name: "Mobile",
+//     price: 50000,
+//     image: "https://images.unsplash.com/photo-1600087626014-e652e18bbff2?q=80&w=928&auto=format&fit=crop",
+//     page: "product-mobile2.html"
+//   },
+//   {
+//     id: "shoes1",
+//     name: "Nike",
+//     price: 9005,
+//     image: "https://images.unsplash.com/photo-1628202926206-c63a34b1618f?q=80&w=774&auto=format&fit=crop",
+//     page: "product-Headphones.html"
+//   },
+//     {
+//     id: "shoes2",
+//     name: "Nike",
+//     price: 7000,
+//     image: "https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?q=80&w=928&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//     page: "product-shoes2.html"
+//   }
   
-];
+// ];
